08a239113e32836f93eb7b00d3624c691bd0bc75
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2024-01-06 17:38:02 +0100

    complete setup

--- src/index.js
+++ src/index.js
@@ -1,39 +1,13 @@
+require("dotenv").config();
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const cors = require("cors");
+const middlewares = require("./middlewares");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    optionsSuccessStatus: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "http://localhost:3000",
-      "Access-Control-Allow-Credentials": true,
-    },
-    credentials: true,
-  })
-);
-
-app.use(
-  session({
-    name: "MySession",
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: false, // Default: true
-    cookie: { secure: false, maxAge: 60000, httpOnly: true },
-    rolling: false, // Default: false
-  })
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(cookieParser());
+middlewares(app);
 
 app.get("/", (req, res) => {
-  console.log("Cookies: ", req.cookies["connect.sid"]);
+  console.log("Cookies: ", req.cookies["ticketswift"]);
   console.log("Session: ", req.session);
   console.log("Session ID: ", req.sessionID);
   req.session.views = req.session.views ? req.session.views + 1 : 1;
